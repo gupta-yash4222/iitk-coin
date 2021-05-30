6517be65d7eabfdf336bc57a424ec419dc00b7e3
gupta-yash4222 <guptayash4222@gmail.com>
2021-05-30 17:20:48 +0530

    Final commit

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import(
 	"database/sql"
 	_ "github.com/mattn/go-sqlite3"
-	"fmt"
 )
 
 type user struct{
@@ -17,16 +16,6 @@ func AddUserData(stmt *sql.Stmt, data user){
 
 }
 
-func FetchData(db *sql.DB){
-	var data user
-	rows, _ := db.Query("SELECT rollno, name FROM User")
-
-	for rows.Next(){
-		rows.Scan(&data.rollno, &data.name)
-		fmt.Printf("rollno: %d, name: %s\n", data.rollno, data.name)
-	}
-}
-
 func main(){
 
 	database, _ := sql.Open( "sqlite3", "./user_details.db")
@@ -37,9 +26,8 @@ func main(){
 	stmt, _ := database.Prepare("INSERT INTO User (rollno, name) VALUES (?, ?)")
 
 	data := user{rollno: 190998, name: "Yash Gupta"}
-	AddUserData(stmt, data)
 
-	FetchData(database)
+	AddUserData(stmt, data)
 
 
 }
