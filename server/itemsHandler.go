1a8bf9fb97e152d3ce209070f56b41e61c1badac
gupta-yash4222 <guptayash4222@gmail.com>
2021-07-27 03:01:53 +0530

    Modified code

--- server/itemsHandler.go
+++ server/itemsHandler.go
@@ -23,6 +23,28 @@ func AddItem(w http.ResponseWriter, r *http.Request) {
 	var inputData model.Item
 	var res model.Response
 
+	claims, err := ValidateUser(r)
+
+	if err != nil {
+		if err.Error() == "Token is either expired or not active yet" {
+			res.Error = "Session expired. Login again."
+			json.NewEncoder(w).Encode(res)
+			return
+		}
+
+		res.Error = err.Error()
+		res.Result = "Verification aborted"
+		json.NewEncoder(w).Encode(res)
+		return
+	}
+
+	if !claims.Admin {
+		res.Error = "User not authorized to add items into the inventory"
+		res.Result = "Permission denied"
+		json.NewEncoder(w).Encode(res)
+		return 
+	}
+
 	body, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 
